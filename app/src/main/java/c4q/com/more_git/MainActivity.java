f3944b03e5d43e12bf1851f9712decf99b08ac65
Darnell O <DarnellOtto@ac.c4q.nyc>
2017-11-21 17:35:09 -0500

    initial commit

---
+++ app/src/main/java/c4q/com/more_git/MainActivity.java
@@ -0,0 +1,20 @@
+package c4q.com.more_git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+    }
+}
